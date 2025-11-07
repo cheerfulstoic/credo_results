4d0e0fa41a424b6b2439c9818bf3d49e09b3d763
Brian Underwood <public@brian-underwood.codes>
2025-11-07 11:52:00 +0100

    fix: Details

--- mix.exs
+++ mix.exs
@@ -1,11 +1,11 @@
-defmodule CredoResultTypes.MixProject do
+defmodule CredoResults.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :credo_result_types,
+      app: :credo_results,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
--- test/credo_result_types_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CredoResultTypesTest do
-  use ExUnit.Case
-  doctest CredoResults
-
-  test "greets the world" do
-    assert CredoResults.hello() == :world
-  end
-end
